184f1465e70fc887e26e06c84989890110dc015a
casey van-roose <kcvanroose@gmail.com>
2018-07-14 18:59:59 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,10 @@
 def my_select(collection)
- # your code here!
+  if block_given?
+    i = 0
+    while i < collection.length
+    if new_items = yield(collection[i])
+    i = i + 1
+    end
+  new_items
+  end
 end
